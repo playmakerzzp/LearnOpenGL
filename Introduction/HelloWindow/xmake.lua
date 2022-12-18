518191ccacabf160500f36449a7d9d4a24417257
playmaker <1344085659@qq.com>
2022-12-18 20:55:05 +0800

    update xmake conf

--- Introduction/HelloWindow/xmake.lua
+++ Introduction/HelloWindow/xmake.lua
@@ -1,6 +1,4 @@
 target("HelloWindow")
     set_kind("binary")
-    add_includedirs("src")
     add_files("src/*.cpp")
-    add_packages("glad","glfw","glm")
-    set_rundir("$(projectdir)")
+    add_packages("glad","glfw")
